5d6110de957221b69d4dea36148886abcff9203d
EOlaw <Emmanuel.ao@outlook.com>
2023-04-28 01:57:36 -0500

    updated a json

--- server.js
+++ server.js
@@ -18,6 +18,11 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.use('/img', express.static(__dirname + 'public/img')) //to load images
 
 
+app.get('/janay', (req, res) => {
+    res.render('janay')
+})
+
+//Making JSON file readable
 const readings = JSON.parse(
     fs.readFileSync(`${__dirname}/api/daily_reading.json`)
 )
@@ -32,6 +37,27 @@ app.get('/api/v1/readings', (req, res) => {
         }
     })
 })
+app.post('/api/v1/readings', (req, res) => {
+    //console.log(req.body)
+    const newId = readings[readings.length - 1].id + 1;
+    const newReadings = Object.assign({ id: newId }, req.body);
+
+    readings.push(newReadings);
+    fs.writeFile(`${__dirname}/api/daily_reading.json`, JSON.stringify(readings), err => {
+        res.status(201).json({
+            status: 'success',
+            data: { 
+                reading: newReadings
+            }
+        })
+    })
+})
+
+//app.get('/api/v1/readings/:id', (req, res) => {
+    //console.log(req.params)
+//})
+
+
 app.get('/', (req, res) => {
     res.status(404).render('welcome')
 })
@@ -40,6 +66,6 @@ const date = new Date();
 console.log(date)
 
 
-app.listen(3300, () => {
-    console.log('listening on port http://localhost:3300')
+app.listen(4400, () => {
+    console.log('listening on port http://localhost:4400')
 })
